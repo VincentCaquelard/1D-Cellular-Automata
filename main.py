85e31d80a256e5da401ccc758ea54f598700fea6
Caquelard Vincent <vincent.caquelard@worldline.com>
2023-03-13 15:08:09 +0100

    Adding initial_state parameter

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ parser.add_argument('-w', '--width',default=100)
 parser.add_argument('-n', '--nb_of_iterations',default=300) 
 parser.add_argument('-n_state', '--nb_of_states',default=3) 
 parser.add_argument('-r', '--rule',default="202112100110202000122012211") 
+parser.add_argument('-o', '--out',default="out.png") 
+parser.add_argument('-init', '--initial_state',default="2") 
 parser.add_argument('--random_rule', action='store_true')
 parser.add_argument('--border_mode', action='store_false')
 
@@ -19,39 +21,6 @@ def get_random_rule(n_state):
 	for k in range(pow(n_state,3)):
 		rule+=str((random.randint(0,n_state-1)))
 	return rule
-
-args = parser.parse_args()
-# Define the dimensions of the image
-width = int(args.width)
-nb_it = int(args.nb_of_iterations)
-
-n_state=int(args.nb_of_states)
-if args.random_rule:
-	rule=get_random_rule(n_state)
-	print("Random rule: ",rule)
-else:
-	rule= args.rule
-
-if len(rule)!= pow(n_state,3):
-	sys.exit(f"Error: rule size: {len(rule)} incompatible with number of states: {n_state}")
-
-#rule= "012121121112210000222102010"
-#rule= "0210211111111120021021021021"
-#rule= "210211121000120021021221021"
-rule_spaghetti= "110211121001120021021221002"
-rule_cool="110211121001120021021201002"
-#rule="220211121001120021021201002"
-#rule_4="202112100110222000000012211"
-#rule="202112100110202000122012211"
-
-
-
-world = [0] * width
-world[width//2]=2
-
-loop_mode=args.border_mode
-
-timeline = world
 
 def rand_palette():
 	p_l=["crest","viridis","YlOrBr","Spectral","flare","cubehelix","rocket","magma","hls"]
@@ -64,6 +33,52 @@ def _get_config(neighbors):
 	for k in range(l):
 		res += pow(n_state,(l-1)-k)*neighbors[k]
 	return res
+
+def _get_world(init_pattern):
+	pt_len = len(init_pattern) # len of the init pattern
+	world = [0] * width
+	shift=(width//2)-pt_len
+	for k in range(pt_len):
+		world[shift+k]=int(init_pattern[k])
+	return world
+
+args = parser.parse_args()
+# Define the dimensions of the image
+width = int(args.width)
+nb_it = int(args.nb_of_iterations)
+n_state=int(args.nb_of_states)
+
+if args.random_rule:
+	rule=get_random_rule(n_state)
+	print("Random rule: ",rule)
+else:
+	rule= args.rule
+if len(rule)!= pow(n_state,3):
+	sys.exit(f"Error: rule size: {len(rule)} incompatible with number of states: {n_state}")
+
+world= _get_world(args.initial_state)
+
+#===========SOME COOL 3-state rule===========
+#rule= "012121121112210000222102010"
+#rule= "0210211111111120021021021021"
+#rule= "210211121000120021021221021"
+#rule_spaghetti= "110211121001120021021221002"
+#rule_cool="110211121001120021021201002"
+#rule="220211121001120021021201002"
+#rule_4="202112100110222000000012211"
+#rule="202112100110202000122012211"
+
+#============SOME 4-state rules==============
+#->3321222303313302311032132200032220022133033331000103013121220322
+
+
+
+
+loop_mode=args.border_mode
+
+timeline = world
+
+
 
 for i in range(nb_it):
 	#print(world)
@@ -93,4 +108,4 @@ img = Image.new('RGB', (width, nb_it+1))
 img.putdata(pixels)
 
 # Save the image to a file
-img.save('test.png')
+img.save(args.out)
